625c4ff4d58649d81ce239722a6fdef5106d65cb
Marco Dalprato <marcodalprato@icloud.com>
2018-10-08 21:13:48 +0200

    1.0.1 -  First commit

--- LoadImageSwiftForMac/ViewController.swift
+++ LoadImageSwiftForMac/ViewController.swift
@@ -9,7 +9,9 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var imagePath: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,5 +25,38 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func loadImage(_ sender: Any) {
+        
+        // Create the dialog
+        
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a .jpg file";
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = true;
+        dialog.allowsMultipleSelection = false;
+        dialog.allowedFileTypes        = ["jpg"]; // supported files (for multiple use ', "png"')
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
+            
+            if (result != nil) {
+                let path = result!.path
+                
+                imagePath.stringValue = path // set the text of the imageParth textbox
+                
+                if let image = NSImage(byReferencingFile: path){
+                    imageView?.image = image // load the image
+                }
+                
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
+        }
+        
+    }
 }
 
