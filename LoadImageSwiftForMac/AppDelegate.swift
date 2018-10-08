6a10d10c63d257db75790848ba25c38dfafceb7d
Marco Dalprato <marcodalprato@icloud.com>
2018-10-08 21:08:37 +0200

    Initial Commit

---
+++ LoadImageSwiftForMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LoadImageSwiftForMac
+//
+//  Created by Marco Dalprato on 08/10/2018.
+//  Copyright © 2018 Marco Dalprato. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LoadImageSwiftForMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LoadImageSwiftForMac
+//
+//  Created by Marco Dalprato on 08/10/2018.
+//  Copyright © 2018 Marco Dalprato. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
